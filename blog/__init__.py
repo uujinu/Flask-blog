5e009f540b52450a63562a3a4de23eaba2e793d5
youjin Cho <ud071233@gmail.com>
2022-05-06 00:10:59 +0900

    Feat: blueprint 객체 등록

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,4 +3,11 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(auth, url_prifix='/')
+    app.register_blueprint(views, url_prifix='/')
+
     return app
