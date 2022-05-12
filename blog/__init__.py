86bcd06b42d63e6d7f0c4069e291af11d8f40819
youjin Cho <ud071233@gmail.com>
2022-05-12 15:44:11 +0900

    Feat: flask_restx를 이용한 라우팅

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
+from flask_restx import Api
+from .route.main import Main
+from .route.user import User
 
 
 def create_app():
     app = Flask(__name__)
+    api = Api(app)
 
-    from .auth import auth
-    from .views import views
-
-    app.register_blueprint(auth, url_prifix='/')
-    app.register_blueprint(views, url_prifix='/')
+    api.add_namespace(Main, '/main')
+    api.add_namespace(User, '/user')
 
     return app
